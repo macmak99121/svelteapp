c9bc5ad1cfc3336d8f525121b64c3e8bd6e9981a
maciekmaka <maciekmaka1337@gmail.com>
2019-10-21 13:34:36 +0200

    commit1

---
+++ maciek/src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+
+
+	const customStrings = [elementName="Kexchoklad", elementName="Powerking", elementName="Kong Strong", elementName="Redbull" ];
+
+	function showStrings()
+	{
+		window.alert("Här står det: " + document.getElementById("t4").innerHTML);
+	}
+
+
+</script>
+
+<style>
+	h1
+	{
+		color: purple;
+	}
+</style>
+
+
+{#each customStrings as text}
+	<p id="t4" on:click={()=>showStrings(this)}>{text}</p>
+{/each}
